b3c81d0c42ca60e1dca3e2f20a89fcfdc47a1ac4
philcho <Phil Cho>
2017-11-10 16:26:47 -0800

    Add jquery. Rename Counter file to App

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import $ from 'jquery';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  getPrecipData() {
+
+  }
+
+  render() {
+    return (
+      <div>App here. Nice to meet you.</div>
+    );
+  }
+}
+
+export default App;
