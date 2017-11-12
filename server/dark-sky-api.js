68a5937ff606ed2973138007746b3dbe88d9cf4f
philcho <Phil Cho>
2017-11-12 07:42:56 -0800

    Change default location to SF.

--- server/dark-sky-api.js
+++ server/dark-sky-api.js
@@ -3,13 +3,14 @@ const request = require('request');
 const DARKSKY_KEY = require('../config.js').DARKSKY_KEY;
 
 // Lat/longs
+  // Hack Reactor: 37.7836924,-122.4111606
   // Portland International Airport: 45.5897694,-122.5972882
   // Mariahilfplatz in Munich: 48.120226,11.5613233
 
 var getPrecipData = function(day, callback) {
   var options = {
     method: 'GET',
-    uri: `https://api.darksky.net/forecast/${DARKSKY_KEY}/48.120226,11.5613233,${day}T00:00:00-8000?exclude=currently,hourly,flags`
+    uri: `https://api.darksky.net/forecast/${DARKSKY_KEY}/37.7836924,-122.4111606,${day}T00:00:00-8000?exclude=currently,hourly,flags`
   };
 
   request(options, function(err, res, body) {
