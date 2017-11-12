ae0aba6444002aa684c3d6d5e796c9fb316237f0
philcho <Phil Cho>
2017-11-11 17:31:46 -0800

    Change default location to Munich

--- server/dark-sky-api.js
+++ server/dark-sky-api.js
@@ -2,10 +2,14 @@ const express = require('express');
 const request = require('request');
 const DARKSKY_KEY = require('../config.js').DARKSKY_KEY;
 
+// Lat/longs
+  // Portland International Airport: 45.5897694,-122.5972882
+  // Mariahilfplatz in Munich: 48.120226,11.5613233
+
 var getPrecipData = function(day, callback) {
   var options = {
     method: 'GET',
-    uri: `https://api.darksky.net/forecast/${DARKSKY_KEY}/42.3601,-71.0589,${day}T00:00:00-8000?exclude=currently,hourly,flags`
+    uri: `https://api.darksky.net/forecast/${DARKSKY_KEY}/48.120226,11.5613233,${day}T00:00:00-8000?exclude=currently,hourly,flags`
   };
 
   request(options, function(err, res, body) {
